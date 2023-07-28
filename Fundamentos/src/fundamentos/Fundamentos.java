a97b8d0629681e7461434cf31338d516de819e62
Nathalia Oliveira <nathalia.goncalves3@aluno.senai.br>
2023-07-28 10:49:09 -0300

    Criando variaveis em Java

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -14,7 +14,13 @@ public class Fundamentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Declaração de variáveis 
+        String nome = "Nathalia";
+        int idade = 18;
+        boolean estudando = true;
+        double media = 5.5;
+        
     }
     
 }
